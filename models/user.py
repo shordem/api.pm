12f97df6275873ec147636a2e5170ee7be90bd18
Horlakz <officialhorlakz@gmail.com>
2024-04-25 14:57:19 +0100

    Add relationships to User model in models/user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,18 @@
 from sqlalchemy import Column, String, Boolean
+from sqlalchemy.orm import relationship
 
-from models.base import BaseModel
+from models import base
 
-class User(BaseModel):
+
+class User(base.BaseModel):
     first_name = Column(String)
     last_name = Column(String)
     username = Column(String, unique=True)
     email = Column(String, unique=True)
     is_email_verified = Column(Boolean, default=False)
     password = Column(String)
+
+    organizations = relationship("Organization", back_populates="owner")
+    members = relationship("Member", back_populates="user")
+    todos = relationship("Todo", back_populates="creator")
+    notes = relationship("Note", back_populates="creator")
