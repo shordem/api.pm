ad60efc4d99ca1780deaa3a2572a05bc4c4e42a5
Horlakz <officialhorlakz@gmail.com>
2024-04-25 16:31:43 +0100

    Refactor organization service to include user verification in add_member_to_organization endpoint

--- services/organization.py
+++ services/organization.py
@@ -7,10 +7,22 @@ from models.member import Member
 from models.user import User
 from schemas.organization import OrganizationCreate
 from schemas.user import User as UserSchema
+from services.user import get_user_by_email
 
 
 def get_organization(db: Session, organization_id: str):
-    return db.query(Organization).filter(Organization.id == organization_id).first()
+    org = db.query(Organization).filter(Organization.id == organization_id).first()
+    if not org:
+        raise Exception("Organization not found")
+    return org
+
+
+def find_by_member(db: Session, user_id: str, organization_id: str):
+    return (
+        db.query(Member)
+        .filter(Member.user_id == user_id, Member.organization_id == organization_id)
+        .first()
+    )
 
 
 def get_organizations_by_member(db: Session, user_id: str):
@@ -49,4 +61,20 @@ def create_organization(db: Session, org: OrganizationCreate):
     new_member = Member(user_id=org.owner_id, organization_id=new_org.id)
     db.add(new_member)
 
+    db.commit()
+
+
+def add_member_to_organization(db: Session, organization_id: str, user_email: str):
+    user = get_user_by_email(db, user_email)
+    if not user.is_email_verified:
+        raise Exception("User is not verified")
+
+    get_organization(db, organization_id)
+
+    member = find_by_member(db, user.id, organization_id)
+    if member:
+        raise Exception("User is already a member of the organization")
+
+    new_member = Member(user_id=user.id, organization_id=organization_id)
+    db.add(new_member)
     db.commit()
