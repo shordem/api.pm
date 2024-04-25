0e78823460e5dace487aeb1db3e5eb9a80660e0d
Horlakz <officialhorlakz@gmail.com>
2024-04-25 17:26:12 +0100

    Refactor TokenData model in authentication.py

--- schemas/authentication.py
+++ schemas/authentication.py
@@ -1,15 +1,18 @@
 from pydantic import BaseModel
 
+
 class Token(BaseModel):
     access_token: str
 
 
 class TokenData(BaseModel):
-    user_id: str | None = None
+    user_id: str
+
 
 class LoginData(BaseModel):
     username: str
     password: str
+
 
 class VerifyEmail(BaseModel):
     email: str
