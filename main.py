b6372652e6532d9651843f213c5e2c8624d0aba6
Horlakz <officialhorlakz@gmail.com>
2024-04-25 14:58:06 +0100

    Refactor main.py to include organization routes and handlers

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from fastapi import FastAPI
 
-from routes import authentication, user
-from models.user import User
+from routes import (
+    authentication,
+    user as user_route,
+    organization as organization_route,
+)
+from config.database import Base
 from config.database import engine
 
 
-User.__base__.metadata.create_all(engine)  
+Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(
+    version="0.1.0",
+    title="Todo App",
+    description="A simple todo app",
+    root_path="/v1",
+)
 
 app.include_router(authentication.router)
-app.include_router(user.router)
+app.include_router(user_route.router)
+app.include_router(organization_route.router)
+
 
 @app.get("/")
 def root():
