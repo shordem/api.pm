6109126f1e12ede494ea542ddf7edf9ec468f8a0
Horlakz <officialhorlakz@gmail.com>
2024-04-25 16:31:27 +0100

    Add get_user_by_email function to services/user.py

--- services/user.py
+++ services/user.py
@@ -2,5 +2,13 @@ from sqlalchemy.orm import Session
 
 from models.user import User
 
+
 def get_user(db: Session, user_id: str):
     return db.query(User).filter(User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    user = db.query(User).filter(User.email == email).first()
+    if not user:
+        raise Exception("User not found")
+    return user
