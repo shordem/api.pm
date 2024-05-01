d9bb3ebc0ac6abd731b56ceb957d8863ae9bb0c0
Horlakz <officialhorlakz@gmail.com>
2024-05-01 23:30:13 +0100

    Add permission check function in permission.py

---
+++ dependencies/permission.py
@@ -0,0 +1,57 @@
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from dependencies.user import get_current_verified_user
+from schemas.user import User
+from schemas.authentication import TokenData
+from dependencies.db import get_db
+from models.organization import Organization
+from models.user_organization import UserOrganization
+from models.permission import Permission
+from models.role_permission import RolePermission
+
+
+def check_permission(
+    db: Session, user: User, organization_id: str, required_permission: str
+):
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    organization = (
+        db.query(Organization).filter(Organization.id == organization_id).first()
+    )
+    if organization is None:
+        raise HTTPException(status_code=404, detail="Organization not found")
+
+    user_organization = (
+        db.query(UserOrganization)
+        .filter(UserOrganization.user_id == user.id)
+        .filter(UserOrganization.organization_id == organization.id)
+        .first()
+    )
+
+    if user_organization is None:
+        raise HTTPException(
+            status_code=404, detail="User is not a member of the organization"
+        )
+
+    permission = (
+        db.query(Permission).filter(Permission.name == required_permission).first()
+    )
+
+    if permission is None:
+        raise HTTPException(status_code=404, detail="Permission not found")
+
+    role_permission = (
+        db.query(RolePermission)
+        .filter(RolePermission.role_id == user_organization.role_id)
+        .filter(RolePermission.permission_id == permission.id)
+        .first()
+    )
+
+    if role_permission is None:
+        raise HTTPException(
+            status_code=403, detail="User does not have the required permission"
+        )
