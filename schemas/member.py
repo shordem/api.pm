14c4dba145815f713d819f0691ade6d29aac86c7
Horlakz <officialhorlakz@gmail.com>
2024-05-02 11:55:12 +0100

    Refactor member schema and add ListMember endpoint

--- schemas/member.py
+++ schemas/member.py
@@ -1,9 +1,15 @@
 from pydantic import BaseModel
 
+from .user import UserSchema
+
 
 class MemberCreate(BaseModel):
     email: str
 
 
 class UpdateMemberRole(BaseModel):
+    role: str
+
+
+class ListMember(UserSchema):
     role: str
