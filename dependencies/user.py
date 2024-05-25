66d8bcebef68339ca537b5e60fd5fc128e83eb6d
Olakotan Daniel <officialhorlakz@gmail.com>
2024-05-25 10:18:57 +0100

    refactor(/dependencies/): updates status in file

--- dependencies/user.py
+++ dependencies/user.py
@@ -21,14 +21,14 @@ async def get_current_user(
         payload = verify_token(token.credentials)
 
         if not isinstance(payload, dict):
-            raise HTTPException(status_code=400, detail="Invalid token")
+            raise HTTPException(status_code=401, detail="Invalid token")
 
         user_id: str = payload.get("sub")
         if user_id is None:
-            raise HTTPException(status_code=400, detail="Invalid token")
+            raise HTTPException(status_code=401, detail="Invalid token")
         token_data = TokenData(user_id=user_id)
     except JWTError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=401, detail=str(e))
 
     user = get_user(db=db, user_id=token_data.user_id)
 
@@ -39,5 +39,5 @@ async def get_current_verified_user(
     current_user: Annotated[UserSchema, Depends(get_current_user)],
 ):
     if current_user.is_email_verified is False:
-        raise HTTPException(status_code=400, detail="Email not verified")
+        raise HTTPException(status_code=403, detail="Email not verified")
     return UserSchema.model_validate(current_user)
